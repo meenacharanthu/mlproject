82ba987086af9c7667ff28666a68ed2393747688
meenacharanthu02@gmail.com <70911397+meenacharanthu@users.noreply.github.com>
2025-02-23 14:02:59 +0530

    data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataIngestionConfig():
+    train_data_path = os.path.join('artifacts','train.csv')
+    test_data_path = os.path.join('artifacts','test.csv')
+    raw_data_path = os.path.join('artifacts','raw_data.csv')
+    
+
+class DataIngestion():
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        try:
+            logging.info('Data Ingestion Started')
+            df = pd.read_csv('notebook\data\StudentsPerformance.csv')
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False, header=True)
+            logging.info('Raw Data Saved')
+
+            train_set, test_set = train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False, header=True)
+            logging.info('Train and Test Data Saved')
+            logging.info('Data Ingestion Completed')
+
+            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+
+        except Exception as e:
+            logging.error(f'Error in Data Ingestion: {str(e)}')
+            raise CustomException(e,sys)
+
+if __name__ == '__main__':
+    data_ingestion = DataIngestion()
+    data_ingestion.initiate_data_ingestion()
